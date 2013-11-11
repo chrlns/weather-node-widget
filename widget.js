ed61fd71f17eeea3f71c226d963fc77c0819576e
Christian Lins <christian@lins.me>
2013-11-11 13:53:33 +0100

    Fix intentation

--- widget.js
+++ widget.js
@@ -1,54 +1,54 @@
 $(document).ready(function() {
-	geolocate();
+    geolocate();
 
-	if(screen.width > screen.height) { /* landscape */
-		$("#symbol").css("height", "80%");
-	} else {
-		$("#symbol").css("width", "80%");
-	}
+    if(screen.width > screen.height) { /* landscape */
+        $("#symbol").css("height", "80%");
+    } else {
+        $("#symbol").css("width", "80%");
+    }
 
-	var $body = $('body'); // Cache this for performance
+    var $body = $('body'); // Cache this for performance
 
-   	var setBodyScale = function() {
+    var setBodyScale = function() {
         var scaleSource = $body.width(),
             scaleFactor = 0.50,                     
             maxScale = 600,
             minScale = 60; // Tweak these values to taste
 
-	    var fontSize = scaleSource * scaleFactor; // Multiply the width of the body by the scaling factor
+        var fontSize = scaleSource * scaleFactor; // Multiply the width of the body by the scaling factor
 
-	    if (fontSize > maxScale) fontSize = maxScale;
-	    if (fontSize < minScale) fontSize = minScale; // Enforce the minimum and maximums
+        if (fontSize > maxScale) fontSize = maxScale;
+        if (fontSize < minScale) fontSize = minScale; // Enforce the minimum and maximums
 
-	    $('body').css('font-size', fontSize + '%');
-  	};
+        $('body').css('font-size', fontSize + '%');
+    };
 
-	$(window).resize(function(){
-	    setBodyScale();
-	});
+    $(window).resize(function(){
+        setBodyScale();
+    });
 
-	setBodyScale();
+    setBodyScale();
 });
 
 
 function geolocate() {
-	if("geolocation" in navigator) {
-		navigator.geolocation.getCurrentPosition(function(pos) {
-			$.getJSON("/weather?lat=" 
-					+ pos.coords.latitude + "&lon=" 
-					+ pos.coords.longitude,
-					function(data) {
-						var citytemp = $("#loctmp");
-						citytemp.text(
-							  data["name"] 
-						  	+ ", "
-						  	+ Math.round(data["main"]["temp"]));
-						
-						citytemp.append($("<span>&deg;C</span>"));
+    if("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            $.getJSON("/weather?lat=" 
+                    + pos.coords.latitude + "&lon=" 
+                    + pos.coords.longitude,
+                    function(data) {
+                        var citytemp = $("#loctmp");
+                        citytemp.text(
+                              data["name"] 
+                            + ", "
+                            + Math.round(data["main"]["temp"]));
+                        
+                        citytemp.append($("<span>&deg;C</span>"));
 
-						$("#symbol").attr("alt", data["weather"][0]["main"]);
-						$("#symbol").attr("src", "img/weather-" + data["weather"][0]["icon"] + ".svg");
-					});
-		});
-	}
+                        $("#symbol").attr("alt", data["weather"][0]["main"]);
+                        $("#symbol").attr("src", "img/weather-" + data["weather"][0]["icon"] + ".svg");
+                    });
+        });
+    }
 }
