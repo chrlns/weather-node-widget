a01de7b80e7b0aa5ef22be9f37405c1df3bf8624
Christian Lins <christian@lins.me>
2014-05-26 10:13:39 +0200

    Print out error code on getCurrentPosition error

--- widget.js
+++ widget.js
@@ -1,7 +1,9 @@
-$(document).ready(function() {
+/*global $*/
+
+$(document).ready(function () {
     geolocate();
 
-    if(screen.width > screen.height) { /* landscape */
+    if (screen.width > screen.height) { /* landscape */
         $("#symbol").css("height", "80%");
     } else {
         $("#symbol").css("width", "80%");
@@ -9,9 +11,9 @@ $(document).ready(function() {
 
     var $body = $("body"); // Cache this for performance
 
-    var setBodyScale = function() {
+    var setBodyScale = function () {
         var scaleSource = $body.width(),
-            scaleFactor = 0.60,                     
+            scaleFactor = 0.60,
             maxScale = 800,
             minScale = 60; // Tweak these values to taste
 
@@ -23,7 +25,7 @@ $(document).ready(function() {
         $body.css("font-size", fontSize + "%");
     };
 
-    $(window).resize(function(){
+    $(window).resize(function () {
         setBodyScale();
     });
 
@@ -37,33 +39,32 @@ function round(x) {
 
 
 function callWeatherService(pos) {
-    var req = $.ajax({url: "/weather" 
-                    + "?lat=" + round(pos.coords.latitude) 
-                    + "&lon=" + round(pos.coords.longitude)
-                    + "&lang=" + navigator.language.substr(0, 2),
-                    dataType: "json",
-                    timeout: 3000});
+    var req = $.ajax({
+        url: "/weather" + "?lat=" + round(pos.coords.latitude) + "&lon=" + round(pos.coords.longitude) + "&lang=" + navigator.language.substr(0, 2),
+        dataType: "json",
+        timeout: 3000
+    });
 
-            req.success(function(data) {
-                var loctmp = $("#loctmp");
-                loctmp.text(data["name"] + ", " + Math.round(data["main"]["temp"]));
-                loctmp.append($("<span>&deg;C</span>"));
-                var symb = $("#symbol"); 
-                symb.attr("alt", data["weather"][0]["main"]);
-                symb.attr("title", data["weather"][0]["main"]);
-                symb.attr("src", "img/weather-" + data["weather"][0]["icon"] + ".svg");
-            });
+    req.success(function (data) {
+        var loctmp = $("#loctmp");
+        loctmp.text(data["name"] + ", " + Math.round(data["main"]["temp"]));
+        loctmp.append($("<span>&deg;C</span>"));
+        var symb = $("#symbol");
+        symb.attr("alt", data["weather"][0]["main"]);
+        symb.attr("title", data["weather"][0]["main"]);
+        symb.attr("src", "img/weather-" + data["weather"][0]["icon"] + ".svg");
+    });
 
-            req.fail(function() {
-                var loctmp = $("#loctmp");
-                loctmp.text("!! Weather Service Failure !!");
-            });
+    req.fail(function () {
+        var loctmp = $("#loctmp");
+        loctmp.text("!! Weather Service Failure !!");
+    });
 }
 
 
 function geolocate() {
     var url = $.url(document.location.href);
-    if(typeof url.param("lat") !== "undefined" && typeof url.param("lon") !== "undefined") {
+    if (typeof url.param("lat") !== "undefined" && typeof url.param("lon") !== "undefined") {
         var pos = {
             coords: {
                 latitude: url.param("lat"),
@@ -71,19 +72,20 @@ function geolocate() {
             }
         };
         callWeatherService(pos);
-    } else if("geolocation" in navigator) {
+    } else if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition(
-		    callWeatherService, 
-		    function() {
-				console.log("Timeout geolocation");
-				callWeatherService(
-					{
-						coords : {
-							latitude: 52.518611,
-							longitude: 13.408056
-						}
-					});
-            }, 
-            {timeout: 5000});
+            callWeatherService,
+            function () {
+                console.log("getCurrentPosition error = " + error);
+                alert("Could not retrieve position. I'll show weather of Berlin instead...");
+                callWeatherService({
+                    coords: {
+                        latitude: 52.518611,
+                        longitude: 13.408056
+                    }
+                });
+            }, {
+                timeout: 5000
+            });
     }
 }
