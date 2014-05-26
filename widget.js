4a09aafad1b0f028b55ec6aef3da9a21dcc162fb
Christian Lins <christian@lins.me>
2014-05-26 10:25:16 +0200

    geolocate is called on symbol click

--- widget.js
+++ widget.js
@@ -1,4 +1,63 @@
-/*global $*/
+"use strict";
+/*global $,document,navigator*/
+
+function round(x) {
+    return Math.round(x * 10000) / 10000.0;
+}
+
+
+function callWeatherService(pos) {
+    var req = $.ajax({
+        url: "/weather" + "?lat=" + round(pos.coords.latitude) + "&lon=" + round(pos.coords.longitude) + "&lang=" + navigator.language.substr(0, 2),
+        dataType: "json",
+        timeout: 3000
+    });
+
+    req.success(function (data) {
+        var loctmp = $("#loctmp");
+        loctmp.text(data["name"] + ", " + Math.round(data["main"]["temp"]));
+        loctmp.append($("<span>&deg;C</span>"));
+        var symb = $("#symbol");
+        symb.attr("alt", data["weather"][0]["main"]);
+        symb.attr("title", data["weather"][0]["main"]);
+        symb.attr("src", "img/weather-" + data["weather"][0]["icon"] + ".svg");
+    });
+
+    req.fail(function () {
+        var loctmp = $("#loctmp");
+        loctmp.text("!! Weather Service Failure !!");
+    });
+}
+
+function geolocate() {
+    var url = $.url(document.location.href),
+        pos;
+
+    if (url.param("lat") !== undefined && url.param("lon") !== undefined) {
+        pos = {
+            coords: {
+                latitude: url.param("lat"),
+                longitude: url.param("lon")
+            }
+        };
+        callWeatherService(pos);
+    } else if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(
+            callWeatherService,
+            function () {
+                console.log("getCurrentPosition error = " + error);
+                git
+                callWeatherService({
+                    coords: {
+                        latitude: 52.518611,
+                        longitude: 13.408056
+                    }
+                });
+            }, {
+                timeout: 5000
+            });
+    }
+}
 
 $(document).ready(function () {
     geolocate();
@@ -30,61 +89,6 @@ $(document).ready(function () {
     });
 
     setBodyScale();
+
+    $("#symbol").click(geolocate);
 });
-
-
-function round(x) {
-    return Math.round(x * 10000) / 10000.0;
-}
-
-
-function callWeatherService(pos) {
-    var req = $.ajax({
-        url: "/weather" + "?lat=" + round(pos.coords.latitude) + "&lon=" + round(pos.coords.longitude) + "&lang=" + navigator.language.substr(0, 2),
-        dataType: "json",
-        timeout: 3000
-    });
-
-    req.success(function (data) {
-        var loctmp = $("#loctmp");
-        loctmp.text(data["name"] + ", " + Math.round(data["main"]["temp"]));
-        loctmp.append($("<span>&deg;C</span>"));
-        var symb = $("#symbol");
-        symb.attr("alt", data["weather"][0]["main"]);
-        symb.attr("title", data["weather"][0]["main"]);
-        symb.attr("src", "img/weather-" + data["weather"][0]["icon"] + ".svg");
-    });
-
-    req.fail(function () {
-        var loctmp = $("#loctmp");
-        loctmp.text("!! Weather Service Failure !!");
-    });
-}
-
-
-function geolocate() {
-    var url = $.url(document.location.href);
-    if (typeof url.param("lat") !== "undefined" && typeof url.param("lon") !== "undefined") {
-        var pos = {
-            coords: {
-                latitude: url.param("lat"),
-                longitude: url.param("lon")
-            }
-        };
-        callWeatherService(pos);
-    } else if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(
-            callWeatherService,
-            function () {
-                console.log("getCurrentPosition error = " + error);
-                callWeatherService({
-                    coords: {
-                        latitude: 52.518611,
-                        longitude: 13.408056
-                    }
-                });
-            }, {
-                timeout: 5000
-            });
-    }
-}
